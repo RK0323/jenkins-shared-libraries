156d2aaff1ce075a149c0c8ce699808e3b9e8e44
RK0323 <122350741+RK0323@users.noreply.github.com>
2024-10-31 14:48:14 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+def call(String ProjectName, String ImageTag, String DockerHubUser) {
   sh """
-  docker build -t ${DockerHubUser}/${ProjectName}":${ImageTag} .
+  docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} .
   """
 }
